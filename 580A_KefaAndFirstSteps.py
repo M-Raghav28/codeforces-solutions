816866b9dd9b9c122c8e1e17cff43266b58737c7
M.Raghav <mraghav2806@gmail.com>
2026-08-06 20:00:36 +0530

    Added 580A_KefaAndFirstSteps

---
+++ 580A_KefaAndFirstSteps.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+max_len = 1
+current_len = 1
+
+for i in range(1, n):
+    if arr[i] >= arr[i - 1]:
+        current_len += 1
+    else:
+        current_len = 1
+    
+    max_len = max(max_len, current_len)
+
+print(max_len)
